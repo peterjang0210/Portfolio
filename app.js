e62c1db868e5914df05fb8b3cd6b19af34e6c271
Peter Jang <peter.jang@outlook.com>
2019-03-04 19:35:33 -0500

    files intialized

---
+++ app.js
@@ -0,0 +1,11 @@
+// function to display text letter by letter
+// const showText = function (target, message, index, interval) {   
+//     if (index < message.length) {
+//       $(target).append(message[index++]);
+//       setTimeout(function () { showText(target, message, index, interval); }, interval);
+//     }
+//   }
+  
+//   $(function () {
+//     showText("#msg", "Hi! I'm Peter. Do you need a web developer?", 0, 100);   
+//   });
