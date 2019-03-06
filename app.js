e32340d4600f69dbf3686c9d7b2541a1434faf73
Peter Jang <peter.jang@outlook.com>
2019-03-05 19:22:09 -0500

    added click events + proper function calls to make SPA

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
+(function () {
+    $(".about").hide();
+    $(".portfolio").hide();
+})();
+
+const renderHome = function () {
+    $("section").hide();
+    $(".home").show();
+
+    $("h1").empty();
+    showText("h1", "Hi, I'm Peter. I'm a full-stack web developer.", 0, 100);
+}
+
+const renderAbout = function () {
+    $("section").hide();
+    $(".about").show();
+}
+
+const renderPortfolio = function () {
+    $("section").hide();
+    $(".portfolio").show();
+}
+
 // function to display text letter by letter
-// const showText = function (target, message, index, interval) {   
-//     if (index < message.length) {
-//       $(target).append(message[index++]);
-//       setTimeout(function () { showText(target, message, index, interval); }, interval);
-//     }
-//   }
-  
-//   $(function () {
-//     showText("#msg", "Hi! I'm Peter. Do you need a web developer?", 0, 100);   
-//   });
+const showText = function (target, message, index, interval) {
+    if (index < message.length) {
+        $(target).append(message[index++]);
+        setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
+}
+
+showText("h1", "Hi, I'm Peter. I'm a full-stack web developer.", 0, 100);
+
+$(".homeLink").on('click', renderHome);
+$(".aboutLink").on('click', renderAbout);
+$(".portfolioLink").on("click", renderPortfolio);
