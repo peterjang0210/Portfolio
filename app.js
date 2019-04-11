7a598f7bab2f23bc7f59228925af67569ded0e93
peterjang0210 <42555727+peterjang0210@users.noreply.github.com>
2019-04-11 16:40:49 -0400

    Revert "React portfolio"

---
+++ app.js
@@ -0,0 +1,153 @@
+(function () {
+    $("section").hide();
+    $("#home").show();
+})();
+
+let abort = false;
+
+const renderHome = function () {
+    abort = false;
+    $("#about").addClass("swing-out-left-bck");
+    $("#home").addClass("scale-up-center");
+    $("#portfolio").addClass("scale-out-center");
+    $("#home").removeClass("fade-out");
+    setTimeout(function () {
+        $("section").hide();
+        $("#home").show();
+        $("h1").empty();
+        showText("h1", "Hi, I'm Peter. I'm a full-stack web developer.", 0, 100);
+    }, 450);
+}
+
+const renderAbout = function () {
+    $("#home").removeClass("scale-up-center");
+    $("#home").addClass("fade-out");
+    abort = true;
+    setTimeout(function () {
+        $("section").hide();
+        $("#about").removeClass("swing-out-left-bck");
+        $("#about").show();
+    }, 1000);
+}
+
+const renderPortfolio = function () {
+    $("#home").removeClass("scale-up-center");
+    $("#home").addClass("fade-out");
+    abort = true;
+    setTimeout(function () {
+        $("section").hide();
+        $("#portfolio").removeClass("scale-out-center");
+        $("#portfolio").show();
+        $('.hwCarousel').flickity({
+            cellAlign: 'center',
+            contain: true,
+            wrapAround: true,
+            freeScroll: true
+        });
+        
+        $('.projectCarousel').flickity({
+            cellAlign: 'center',
+            contain: true,
+            freeScroll: true,
+            wrapAround: true
+        });
+        $(".carousel-cell-imageHW").hover(changeImageEnterHW, changeImageExitHW);
+        $(".carousel-cell-imageProj").hover(changeImageEnterProj, changeImageExitProj);
+    }, 1000);
+}
+
+// function to display text letter by letter
+const showText = function (target, message, index, interval) {
+    if (index < message.length && abort === false) {
+        $(target).append(message[index++]);
+        setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
+    else {
+        return;
+    }
+}
+
+const renderHWModal = function () {
+    const hwID = parseInt($(this).attr("dataHwID"));
+    console.log(hwID - 1);
+    $(".modal-title").html(`${bootcampHWs[hwID - 1].name}`)
+    $(".modal-body").html(
+        `<div>
+            <p>${bootcampHWs[hwID - 1].description}</p>
+            <a class="modalLink" href=${bootcampHWs[hwID - 1].deployLink}>Deployed</a> | 
+            <a class="modalLink" href=${bootcampHWs[hwID - 1].githubLink}>Github</a>
+        </div>`
+    );
+    $("#modal").modal("show");
+}
+
+const renderProjectModal = function () {
+    const projectID = $(this).attr("dataProjectID");
+    $(".modal-title").html(`${bootcampProjects[projectID - 1].name}`)
+    $(".modal-body").html(
+        `<div>
+            <p>${bootcampProjects[projectID - 1].description}</p>
+            <a class="modalLink" href=${bootcampProjects[projectID - 1].deployLink}>Deployed</a> | 
+            <a class="modalLink" href=${bootcampProjects[projectID - 1].githubLink}>Github</a>
+        </div>`
+    );
+    $("#modal").modal("show");
+}
+
+const changeImageExitHW = function () {
+    const hwID = $(this).attr("dataHwID");
+    $(`.hwImage${$(this).attr("dataHwID")}`).removeClass("fade-in-fwd");
+    if(hwID == 1){
+        $(`.hwImage${hwID}`).attr("src", "./images/stack-letters-letter-handwriting-family-letters-51191.jpeg");
+    }
+    else if(hwID == 2){
+        $(`.hwImage${hwID}`).attr("src", "./images/pexels-photo-357514.jpeg");
+    }
+    else if(hwID == 3){
+        $(`.hwImage${hwID}`).attr("src", "./images/pexels-photo-220076.jpeg");
+    }
+    else if(hwID == 4){
+        $(`.hwImage${hwID}`).attr("src", "./images/pexels-photo-69760.jpeg");
+    }
+    else if(hwID == 5){
+        $(`.hwImage${hwID}`).attr("src", "./images/pexels-photo-270238.png");
+    }
+    else if(hwID == 6){
+        $(`.hwImage${hwID}`).attr("src", "./images/pexels-photo-907607.png");
+    }
+    else if(hwID == 7){
+        $(`.hwImage${hwID}`).attr("src", "./images/smilies-bank-sit-rest-160739.jpeg");
+    }
+}
+
+const changeImageEnterHW = function (){
+    $(`.hwImage${$(this).attr("dataHwID")}`).attr("src", `./images/bootcamphw${$(this).attr("dataHwID")}.png`);
+    $(`.hwImage${$(this).attr("dataHwID")}`).addClass("fade-in-fwd");
+}
+
+const changeImageExitProj = function () {
+    const projID = $(this).attr("dataProjectID");
+    $(`.projImage${projID}`).removeClass("fade-in-fwd");
+    if(projID == 1){
+        $(`.projImage${projID}`).attr("src", "./images/pexels-photo-1021876.jpeg");
+    }
+    else if(projID == 2){
+        $(`.projImage${projID}`).attr("src", "./images/bccLogo.png");
+    }
+    else if(projID == 3){
+        $(`.projImage${projID}`).attr("src", "./images/workInProgress.jpeg");
+    }
+}
+
+const changeImageEnterProj = function (){
+    $(`.projImage${$(this).attr("dataProjectID")}`).attr("src", `./images/bootcampproject${$(this).attr("dataProjectID")}.png`);
+    $(`.projImage${$(this).attr("dataProjectID")}`).addClass("fade-in-fwd");
+}
+
+showText("h1", "Hi, I'm Peter. I'm a full-stack web developer.", 0, 100);
+
+$("#portfolio").on('click', ".carousel-cell-imageHW", renderHWModal);
+$("#portfolio").on('click', ".carousel-cell-imageProj", renderProjectModal);
+$(".homeLink").on('click', renderHome);
+$(".aboutLink").on('click', renderAbout);
+$(".portfolioLink").on("click", renderPortfolio);
---
+++ portfolioModals.js
@@ -0,0 +1,77 @@
+const bootcampHWs = [
+    {
+        name: "Portfolio v1",
+        description: "First iteration of portfolio built using HTML/CSS. Not responsive",
+        technologies: ["HTML", "CSS"],
+        deployLink: "https://peterjang0210.github.io/Basic-Repository",
+        githubLink: "https://github.com/peterjang0210/Basic-Repository",
+        imageLink: "./images/bootcamphw1.png"
+    },
+    {
+        name: "Portfolio v2",
+        description: "Second iteration of portfolio built using HTML/CSS. Made to be mobile responsive using media queries",
+        technologies: ["HTML", "CSS"],
+        deployLink: "https://peterjang0210.github.io/Responsive-Portfolio",
+        githubLink: "https://github.com/peterjang0210/Responsive-Portfolio",
+        imageLink: "./images/bootcamphw2.png"
+    },
+    {
+        name: "Employee Directory",
+        description: "Single page application that made use of self-built jQuery like library to perform DOM manipulation",
+        technologies: ["HTML", "CSS(Bootstrap)", "Javascript"],
+        deployLink: "https://peterjang0210.github.io/Employee-Directory",
+        githubLink: "https://github.com/peterjang0210/Employee-Directory",
+        imageLink: "./images/bootcamphw3.png"
+    },
+    {
+        name: "Stock Search App",
+        description: "Made use of jQuery to perfrom ajax requests to APIs. Retrieved data and dynamically appended to the DOM",
+        technologies: ["Javascript", "jQuery"],
+        deployLink: "https://peterjang0210.github.io/Stock-Search-Application",
+        githubLink: "https://github.com/peterjang0210/Stock-Search-Application",
+        imageLink: "./images/bootcamphw4.png"
+    },
+    {
+        name: "Employee Finder",
+        description: "Used Node.js and Express package to run server for basic backend functions such as routing",
+        technologies: ["jQuery", "Node.js", "Express"],
+        deployLink: "https://lit-brook-93835.herokuapp.com",
+        githubLink: "https://github.com/peterjang0210/EmployeeFinder",
+        imageLink: "./images/bootcamphw5.png"
+    },
+    {
+        name: "Bamazon",
+        description: "Full stack application created using MySQL and Sequelize for database functionality.",
+        technologies: ["Node.js", "MySQL", "Sequelize"],
+        deployLink: "https://secure-caverns-63801.herokuapp.com",
+        githubLink: "https://github.com/peterjang0210/Bamazon",
+        imageLink: "./images/bootcamphw6.png"
+    },
+    {
+        name: "Tiny Improvements",
+        description: "Full stack application created using mongoDB and mongoose for database functionality.",
+        technologies: ["Node.js", "mongoDB", "mongoose"],
+        deployLink: "https://tiny-improvements-hw.herokuapp.com",
+        githubLink: "https://github.com/peterjang0210/Tiny-Improvements",
+        imageLink: "./images/bootcamphw7.png"
+    }
+];
+
+const bootcampProjects = [
+    {
+        name: "Muugle",
+        description: "Front end web app that uses multiple APIs, including spotifyAPI to allow the user to play songs, create playlists, edit playlists, and view information about the song.",
+        technologies: ["CSS", "Javascript", "jQuery"],
+        deployLink: "https://peterjang0210.github.io/Muugle",
+        githubLink: "https://github.com/peterjang0210/Muugle",
+        imageLink: "./images/bootcampproject1.png"
+    },
+    {
+        name: "Bootcamp Connect",
+        description: "Full-stack MERN Application that allows users to register/login, update their own profile information, view other users' profiles, and create posts",
+        technologies: ["React", "MongoDB", "Mongoose"],
+        deployLink: "https://bootcamp-connect.herokuapp.com",
+        githubLink: "https://github.com/peterjang0210/bootcamp-connect",
+        imageLink: "./images/bootcampproject2.png"
+    }
+]
