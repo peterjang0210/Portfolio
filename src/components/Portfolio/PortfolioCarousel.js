86c7beead64bb405fcde28f50f13e4f438c5eab6
Peter Jang <peter.jang@outlook.com>
2020-12-23 19:09:15 -0500

    updates carousel + carousel videos

--- src/components/Portfolio/PortfolioCarousel.js
+++ src/components/Portfolio/PortfolioCarousel.js
@@ -7,46 +7,27 @@ import {
   CarouselCaption,
   Container
 } from "reactstrap";
-import MuugleDemo from "../../images/Muugle.mp4";
-import BootcampConnectDemo from "../../images/BootcampConnect.mp4";
-import FrontDorDemo from "../../images/FrontDor.mp4";
+import fieldServiceApp from "../../images/FieldServiceApp.mp4";
+import announcementBoard from "../../images/AnnouncementBoard.mp4";
 
 const items = [
   {
-    src: MuugleDemo,
-    altText: "Muugle",
-    title: "Muugle",
-    description: "Muugle was the first project that I worked on during the coding bootcamp at Georgia Tech. It is a front end application that takes advantage of the Spotify API and two other APIs to provide the user with a one stop shop for listening to music, curating playlists, and viewing song information/lyrics. It was built using Bootstrap as the framework and jQuery the library of choice for DOM manipulation. The application also uses cookies to store recently played songs locally.",
-    challenge: "The biggest challenge while working on this application was that it was my first time working on a project of this magnitude in groups. Working on my own components without having a direct understanding of how those components would interact with the components built by the other team members was a great challenge to work through and resolve.",
-    githubLink: "https://github.com/peterjang0210/Muugle",
-    deployedLink: "https://peterjang0210.github.io/Muugle/"
-  },
-  {
-    src: BootcampConnectDemo,
-    altText: "Bootcamp Connect",
-    title: "Bootcamp Connect",
-    description: "This is a full-stack MERN (MongoDB, Express.js, React, Node) application designed to be a platform for bootcamp graduates to post helpful resources post graduation. This was the first relatively large scale full-stack application that I created as part of a team. The backend is entirely written in javascript using Express.js and Node to handle server side functions while using Mongoose as the object document mapper to act as middleware between the server and the database.",
-    challenge: "Bootcamp Connect was built using React as the framework of choice on the front end. However, we had learned the fundamentals of React only days before the start of this project, and as a result, there is a significant amount of code that could be refactored to be efficient. Using an unfamiliar framework under a strict time constraint was a signficant challenge that would not have been easy to solve without a team. I definitely grew as a developer from this experience and learned to struggle through documentation and experimentation.",
-    githubLink: "https://github.com/peterjang0210/bootcamp-connect",
-    deployedLink: "https://bootcamp-connect.herokuapp.com"
-  },
-  {
-    src: FrontDorDemo,
-    altText: "FrontDor",
-    title: "FrontDor",
-    description: "FrontDor is a real-time chat application built for small businesses to use for communciation between team members and with customers through sms. This application is also a MERN stack application that was built using feathers.js. A unique feature of this application is that it takes advantage of sockets for faster real-time transfer of information compared to the standard HTTP requests. ",
-    challenge: "The biggest challnge with building this app was that feathers.js was a completely unfamiliar framework that I had to learn independently from scratch. The use of sockets was also a topic that I was not familiar with but was fundamental for the entirety of the project. This was compounded by the fact that my other team members were not familiar with the technology either. This entire project was a wake up call on how much room I had to grow as a developer. Reading through pages and pages of documentation and examples on stack overflow to understand how a component functioned was a humbling experience, but one that I truly appreciated by the end.",
-    githubLink: "https://github.com/timothywadecook/team-chat",
-    deployedLink: "https://frontdor.herokuapp.com/"
-  },
-  {
-    // src: fieldServiceAppDemo,
+    src: fieldServiceApp,
     altText: "Alba 2.0",
     title: "Field Service App",
-    description: "This app is currently in development. This app was developed with the intention of replacing Alba as the primary application used during field service in my congregation. We found that many of the older friends struggled with the smaller text and UI in Alba and that they struggled to navigate efficiently between houses as the addresses were displayed in alphabetical order. Although Alba is a great application, this app is my attempt to fix some of those issues for the older friends in the hall. Firstly, the text and buttons in the UI are much bigger and therefore easier to read. Another feature is that the doors are arranged in a set order so that there is minimal travel time between houses. This application also has a more robust record keeping feature so that the friends are more aware of the householder's circumstances before their call. Currently, this application contains most of the features included in Alba except for the ability to import a large number of addresses at once, auto assigning addresses to already created cards based on their location, and display the addresses on the card on a map. These features are currently in progress.",
-    challenge: "The repository for this project is currently set to private and as such cannot be viewed without requesting access. The live version of the app can be logged in via the username: John Doe and phone number: 1112223333.",
+    description: "This app is currently being rolled out slowly. It was developed to make up for some of the limitations by the alba program used for field service. During the pandemic, we found that as more records were added to each address in Alba, it became more difficult to maintain a clean record history. This app attmpted to make up for some of those shortcomings. Records for each address can be sorted by category (door-to-door, letter, phone) and are displayed in a table that is easy to view. Another feature that was added was a search feature for phone numbers. The function uses regular expressions to search the records for matching phone numbers and informs the user whether the phone number is a duplicate or not. The text and UI elements for the application have also been enlarged for ease of use by the older friends. For the admin, a mass import of addresses, users, and records from an excel file have been added.",
+    challenge: "The repository for this project is currently set to private and as such cannot be viewed without requesting access.",
     githubLink: "https://github.com/peterjang0210/Field-Service-App",
-    deployedLink: "https://github.com/peterjang0210/Field-Service-App"
+    deployedLink: "https://suwanee-field-service.herokuapp.com/"
+  },
+  {
+    src: announcementBoard,
+    altText: "Congregation Announcement Board",
+    title: "Congregation Announcement Board",
+    description: "This app was developed duirng the pandemic to serve as an online announcement board since the kingdom hall could not be accesssed by many of the friends. The app contains tabs for meetings, field service, upcoming events, and other announcements. Zoom links for meetings and field service are included as well as meeting assignments and branch announcments. This application was initially developed with a feature that allowed the friends to sign up for comments during the meetings, but that feature was disabled after it was decided to use the raise hand feature in zoom instead.",
+    challenge: "The repository for this project is currently set to private and as such cannot be viewed without requesting access.",
+    githubLink: "https://github.com/peterjang0210/meetingComments",
+    deployedLink: "http://suwaneejw.herokuapp.com/sk/announcements"
   }
 ];
 
@@ -150,7 +131,7 @@ class PortfolioCarousel extends React.Component {
           activeIndex={activeIndex}
           next={this.next}
           previous={this.previous}
-          interval={24000}
+          interval={60000}
           ride="carousel"
         >
           <CarouselIndicators
